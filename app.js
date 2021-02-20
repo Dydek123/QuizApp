6eef542bbff98082a55530312f29c47d3310f1e1
Dydek123 <mazurkiewicz707@gmail.com>
2021-02-20 15:55:17 +0100

    Delete comments and set port

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const game = require('./routes/game');
 
-
+const port = process.env.PORT || '3000';
 const app = express();
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log('Servers is listening at http://localhost:3000 Lets play a game');
 });
 
